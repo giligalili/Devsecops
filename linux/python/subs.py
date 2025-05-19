9c3d1deafe1bc2423514b495669a233f8570faac
giligalili <giligalili@gmail.com>
2025-05-19 10:53:08 -0400

    sub1

--- linux/python/subs.py
+++ linux/python/subs.py
@@ -20,17 +20,6 @@ enc_key = make_enc_key()
 # יצירת מפתח פענוח
 dec_key = compute_dec_key(enc_key)
 
-# הדפסת המפתחות
-#print("מפתח הצפנה:", enc_key)
-#print("מפתח פענוח:", dec_key)
-
-# def encrypt_text(free_text as i,enc_key as en):
-#     for a in i:
-#         out_text = i.replace(i[a],en[a])
-#         a=+1
-
-#     print(out_text)
-
 def encrypt_text(free_text, enc_key):
     """מצפין טקסט לפי מפתח הצפנה"""
     out_text = ""
@@ -47,18 +36,15 @@ def decrypt_text(out_text, dec_key):
 
     for char in out_text:
         in_text += dec_key.get(char, char)  # החלפה לפי מפתח, אם התו אינו קיים משאירים אותו כמות שהוא
-
-   # print(in_text)
     return in_text
 
-#user_text =''
-user_text = input("Enter Text To Encrypt:")
-# text_to_encrypt = "Hello, World!"
-encrypted_text = encrypt_text(user_text, enc_key)
-decryption_text = decrypt_text(encrypted_text, dec_key)
+def test_all(user_text):
+    user_text = input("Enter Text To Encrypt:")
+    encrypted_text = encrypt_text(user_text, enc_key)
+    decryption_text = decrypt_text(encrypted_text, dec_key)
+    print("Encrypted_Text:", encrypted_text)
+    print("Decryption_Text:", decryption_text)
 
-print("Encrypted_Text:", encrypted_text)
-print("Decryption_Text:", decryption_text)
 
 
 
